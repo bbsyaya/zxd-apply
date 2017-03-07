bee90acc808840958982dfb604cc99f2d1439c28
guoqiang <guoqiang@zhixindu.com>
2017-03-07 16:29:48 +0800

    修改提交借款人相关信息的dubbo接口的返回值为boolean

--- zxd-apply-facade/src/main/java/com/zhixindu/apply/facade/apply/business/DubboApplyWechatBusiness.java
+++ zxd-apply-facade/src/main/java/com/zhixindu/apply/facade/apply/business/DubboApplyWechatBusiness.java
@@ -1,7 +1,12 @@
 package com.zhixindu.apply.facade.apply.business;
 
+import com.zhixindu.apply.facade.apply.bo.ApplyBO;
+
 /**
  * Created by SteveGuo on 2017/3/3.
  */
 public interface DubboApplyWechatBusiness {
+
+    boolean submitLoanApply(ApplyBO applyBO);
+
 }
