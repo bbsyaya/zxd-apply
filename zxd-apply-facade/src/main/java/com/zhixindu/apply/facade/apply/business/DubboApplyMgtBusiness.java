682cdc425df20aceca0a9bcd957088861ae6a7f0
Yulei <yulei@zhixindu.com>
2017-03-16 20:26:24 +0800

    申请资料查询，更换接口名称

--- zxd-apply-facade/src/main/java/com/zhixindu/apply/facade/apply/business/DubboApplyMgtBusiness.java
+++ zxd-apply-facade/src/main/java/com/zhixindu/apply/facade/apply/business/DubboApplyMgtBusiness.java
@@ -15,7 +15,7 @@ public interface DubboApplyMgtBusiness {
      * 获取申请借款信息
      */
 
-    ApplyMgtInfo findApplyInfoByLenderId(Integer lender_id) throws ServiceException;
+    ApplyMgtInfo findApplyInfoByApplyId(Integer apply_id) throws ServiceException;
 
     /**
      * 获取申请借款列表
