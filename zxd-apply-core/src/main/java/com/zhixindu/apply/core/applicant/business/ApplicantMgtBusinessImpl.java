c84ea8bfde075b24d18f6a07f705b64db399d35b
guoqiang <guoqiang@zhixindu.com>
2017-04-12 17:19:16 +0800

    修改错误消息提示

--- zxd-apply-core/src/main/java/com/zhixindu/apply/core/applicant/business/ApplicantMgtBusinessImpl.java
+++ zxd-apply-core/src/main/java/com/zhixindu/apply/core/applicant/business/ApplicantMgtBusinessImpl.java
@@ -4,7 +4,6 @@ import com.zhixindu.apply.core.applicant.dao.ApplicantMapper;
 import com.zhixindu.apply.core.applicant.service.ApplicantService;
 import com.zhixindu.apply.core.constant.ApplyErrorCode;
 import com.zhixindu.apply.facade.applicant.bo.ApplicantBO;
-import com.zhixindu.apply.facade.applicant.bo.ApplicantInfoBO;
 import com.zhixindu.apply.facade.applicant.bo.ApplicantMgtInfo;
 import com.zhixindu.apply.facade.applicant.bo.ApplicantMgtQueryParam;
 import com.zhixindu.apply.facade.applicant.bo.ApplyResultBO;
@@ -48,7 +47,7 @@ public class ApplicantMgtBusinessImpl implements DubboApplicantMgtBusiness {
         Parameters.requireNotNull(applicant_id,"findApplicantInfo applicant_id illegal_param !");
         ApplicantBO applicantBO = applicantMapper.selectByPrimaryKey(applicant_id);
         if(null == applicantBO){
-            throw new ServiceException(ApplyErrorCode.NO_APPLY.getErrorCode(), ApplyErrorCode.NO_APPLY.getDesc());
+            throw new ServiceException(ApplyErrorCode.NO_APPLICANT.getErrorCode(), ApplyErrorCode.NO_APPLICANT.getDesc());
         }
         ApplicantMgtInfo applicantMgtInfo = new ApplicantMgtInfo();
         BeanUtils.copyProperties(applicantBO, applicantMgtInfo);
